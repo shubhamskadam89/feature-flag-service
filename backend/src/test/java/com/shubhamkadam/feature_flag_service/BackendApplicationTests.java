07d059cfff432c5c17add082968422af4fee0537
Shubham Kadam <shubhamskadam89@gmail.com>
2026-08-08 21:15:32 +0530

    feat: initialize spring boot backend project with maven wrapper and docker-compose configuration

---
+++ backend/src/test/java/com/shubhamkadam/feature_flag_service/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubhamkadam.feature_flag_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
