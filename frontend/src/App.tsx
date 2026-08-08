f53bf67c4b9dbaaa3324586943360e27996e27d1
Shubham Kadam <shubhamskadam89@gmail.com>
2026-08-08 21:32:26 +0530

    chore: install frontend dependencies and initialize project CSS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='w-screen h-screen bg-black flex justify-center items-center'>
+        <h1 className='text-6xl text-white font-bold underline'>Feature-Flag-Service</h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
