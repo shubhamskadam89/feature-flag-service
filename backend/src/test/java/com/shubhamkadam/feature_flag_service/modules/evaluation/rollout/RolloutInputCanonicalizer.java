7f69673ebf7e800b2c04bc3fc667e1ca5f674b14
Shubham Kadam <shubhamskadam89@gmail.com>
2026-08-18 19:21:25 +0530

    refactor(evaluation): move rollout components to src/main and keep unit tests in src/test

--- backend/src/test/java/com/shubhamkadam/feature_flag_service/modules/evaluation/rollout/RolloutInputCanonicalizer.java
+++
@@ -1,5 +0,0 @@
-package com.shubhamkadam.feature_flag_service.modules.evaluation.rollout;
-
-public interface RolloutInputCanonicalizer {
-    byte[] canonicalize(String featureKey, String contextKey);
-}
